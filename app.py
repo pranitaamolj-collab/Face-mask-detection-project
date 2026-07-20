4ee1b7230b8474d02c2ef9fcb8c9cd39ebc217fb
pranitaamolj-collab <pranitaamolj@gmail.com>
2026-07-20 23:41:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,10 @@ from PIL import Image
 from tensorflow.keras.models import load_model
 from streamlit_cropper import st_cropper
 
-# Load model
+# Load Model
 model = load_model("face-mask-detector.keras")
+
+st.set_page_config(page_title="Face Mask Detection", page_icon="😷")
 
 st.title("😷 Face Mask Detection Project")
 
@@ -14,14 +16,12 @@ option = st.selectbox("Select Input", ["Image", "Capture"])
 uploaded_file = None
 cropped_img = None
 
-# Upload Image
 if option == "Image":
     uploaded_file = st.file_uploader(
         "Upload Image",
         type=["jpg", "jpeg", "png"]
     )
 
-# Capture Image
 else:
     camera_image = st.camera_input("Capture Image")
 
@@ -35,9 +35,6 @@ else:
             aspect_ratio=(1,1)
         )
 
-        st.image(cropped_img, caption="Captured Image", width=350)
-
-# Detect Button
 if st.button("Detect"):
 
     image = None
@@ -60,17 +57,17 @@ if st.button("Detect"):
         img = image.convert("RGB")
         img = img.resize((150,150))
 
-        img_array = np.array(img).astype(np.float32) / 255.0
+        img_array = np.array(img, dtype=np.float32) / 255.0
         img_array = np.expand_dims(img_array, axis=0)
 
-        # Predict
+        # Prediction
         prediction = model.predict(img_array, verbose=0)
         score = float(prediction[0][0])
 
         st.write(f"### Prediction Score : {score:.4f}")
 
-        # Prediction
-        if score > 0.5:
+        # Result
+        if score <= 0.5:
             st.success("😷 Person is WITH MASK")
         else:
             st.error("❌ Person is WITHOUT MASK")
